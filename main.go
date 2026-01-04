381d249cab69c0f3209ac07e742c608a596acea4
Andrey <kaspersky1236@gmail.com>
2026-01-04 15:59:58 +0400

    second commit

--- main.go
+++ main.go
@@ -14,20 +14,21 @@ const (
 	
 )
 
+// TODO: chtoxnel het gnalu hnaravorutyun.
 func rand_walk(startingPositionX int32, startingPositionY int32, color rl.Color) {
 	escDontPressed := true
 	fps := 0 
 	for escDontPressed {
-		if rl.IsKeyPressed(rl.KeyEscape) || fps == 800 { escDontPressed = false }	
+		if rl.IsKeyPressed(rl.KeyEscape) || fps == 20000 { escDontPressed = false }	
 		
 		PositionX, PositionY := startingPositionX, startingPositionY
 
 		rand_num := rand.Intn(4) 
 
-		if rand_num == 0 { startingPositionX = PositionX - 20 }
-		if rand_num == 1 { startingPositionX = PositionX + 20 }
-		if rand_num == 2 { startingPositionY = PositionY + 20 }
-		if rand_num == 3 { startingPositionY = PositionY - 20 }
+		if rand_num == 0 { startingPositionX = PositionX - 2 }
+		if rand_num == 1 { startingPositionX = PositionX + 2 }
+		if rand_num == 2 { startingPositionY = PositionY + 2 }
+		if rand_num == 3 { startingPositionY = PositionY - 2 }
 
 		rl.DrawLine(startingPositionX, startingPositionY, PositionX, PositionY, color)
 		fps++
@@ -37,21 +38,20 @@ func rand_walk(startingPositionX int32, startingPositionY int32, color rl.Color)
 func main() {
 	rl.InitWindow(1300, 600, "Random Walk")
 	
-	rl.SetTargetFPS(2)
+	rl.SetTargetFPS(1)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		
 		rl.ClearBackground(rl.Black)
 		// logic is here
+		rand_walk(555, 300, rl.Green)
+		rand_walk(500, 300, rl.Blue)
+		rand_walk(500, 300, rl.Red)
+		rand_walk(500, 300, rl.Yellow)
 
-		rand_walk(400, 400, rl.Green)
-		rand_walk(400, 400, rl.Blue)
-		rand_walk(400, 400, rl.Red)
-		rand_walk(400, 400, rl.Yellow)
-
-		rand_walk(400, 400, rl.NewColor(128, 0, 128, 255))
-		rand_walk(400, 400, rl.NewColor(255, 69, 0, 255))
+		rand_walk(500, 300, rl.NewColor(128, 0, 128, 255))
+		rand_walk(500, 300, rl.NewColor(255, 69, 0, 255))
 
 		rl.EndDrawing()
 	}
